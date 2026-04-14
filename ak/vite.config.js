68c27a112a27e3fed3608860c29efe58b6b229bc
ankitverma395 <ankitkumar19739@gmail.com>
2026-04-14 10:02:14 +0530

    Refactor vite.config.js by removing preview config
    Removed preview configuration and adjusted formatting.

--- ak/vite.config.js
+++ ak/vite.config.js
@@ -47,12 +47,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173
-  },
-
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    strictPort: true,
-    allowedHosts: 'all'
   }
 })
