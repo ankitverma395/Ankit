189c03c3c01fc0c3c476ab15081903ee30f433dc
ankitverma395 <ankitkumar19739@gmail.com>
2026-04-20 01:52:07 +0530

    Refactor main.jsx by removing PWA registration
    Removed service worker registration for production.

--- aa/src/main.jsx
+++ aa/src/main.jsx
@@ -4,15 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-import { registerSW } from 'virtual:pwa-register'
-
-// ✅ SAFE SW (no crash in build)
-if (import.meta.env.PROD) {
-  registerSW({
-    immediate: true
-  })
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
